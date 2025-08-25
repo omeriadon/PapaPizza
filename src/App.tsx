fdbadf1af9626cd37a6cd1e455d594b5dd8b53f7
omeriadon <adon.omeri@student.education.wa.edu.au>
2025-08-25 20:56:07 +0800

    Initial commit, toolbar done

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+
+    <img src="/tomato.png" alt="" />
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+      <div>monkey</div>
+    </>
+  );
+}
+
+export default App;
