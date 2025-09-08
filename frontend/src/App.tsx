1ea3e6930ff84466c97b8d96a3d038983cf596f7
omeriadon <adon.omeri@student.education.wa.edu.au>
2025-09-08 11:25:09 +0800

    change home route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css"
 import Order from "./pages/Order";
-import Home from "./pages/Home";
 import SalesSummary from "./pages/SalesSummary";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Order />} />
         <Route path="/order" element={<Order />} />
         <Route path="/salesSummary" element={<SalesSummary />} />
       </Routes>
